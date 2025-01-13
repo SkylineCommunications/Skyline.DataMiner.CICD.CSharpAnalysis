ed9fe49e102dc70c9c6db6f412225b7fd0a3032b
Arne Maes <arne.maes@skyline.be>
2025-01-13 11:59:14 +0100

    Added namespace property to ClassClass Added QualifiedName handling to the RoslynHelper Added Unit test for the QualifiedName handling with an extra test on properties

--- CommonTests/CheckPropertyTests.cs
+++ CommonTests/CheckPropertyTests.cs
@@ -143,6 +143,127 @@ public static class QAction
             }
         }
 
+        [TestMethod]
+        public void CheckProperty_Types()
+        {
+            List<string?> results = new List<string?>();
+
+            const string programText =
+   @"
+public static class QAction
+{
+    [Description(""Property 1's custom description"")]
+	public string Prop1 { get; set; }
+    [Required]
+	public Guid Prop2 { get; set; }
+    [Required]
+    [Description(""Property 3's custom description"")]
+	public List<string> Prop3 { get; set; } = new List<string>();
+	public Skyline.DataMiner.Net.IConnection Prop4 { get; }
+}";
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(programText);
+
+            QActionAnalyzer analyzer = new QActionAnalyzer(CheckProperties);
+            RoslynVisitor parser = new RoslynVisitor(analyzer);
+
+            parser.Visit(syntaxTree.GetRoot());
+
+            results.RemoveAll(String.IsNullOrWhiteSpace);
+
+            if (results.Count > 0)
+            {
+                throw new AssertFailedException(String.Join(Environment.NewLine, results));
+            }
+
+            void CheckProperties(PropertyClass property)
+            {
+                results.Add(Property1(property));
+                results.Add(Property2(property));
+                results.Add(Property3(property));
+                results.Add(Property4(property));
+            }
+
+            string? Property1(PropertyClass property)
+            {
+                if (property.Name != "Prop1")
+                {
+                    return null;
+                }
+
+                try
+                {
+                    property.Type.Should().NotBeNullOrEmpty();
+                    property.Type.Should().Be("string");
+                }
+                catch (AssertFailedException e)
+                {
+                    return $"[Prop1] {e.Message}";
+                }
+
+                return String.Empty;
+            }
+
+            string? Property2(PropertyClass property)
+            {
+                if (property.Name != "Prop2")
+                {
+                    return null;
+                }
+
+                try
+                {
+                    property.Type.Should().NotBeNullOrEmpty();
+                    property.Type.Should().Be("Guid");
+                }
+                catch (AssertFailedException e)
+                {
+                    return $"[Prop2] {e.Message}";
+                }
+
+                return String.Empty;
+            }
+
+            string? Property3(PropertyClass property)
+            {
+                if (property.Name != "Prop3")
+                {
+                    return null;
+                }
+
+                try
+                {
+                    property.Type.Should().NotBeNullOrEmpty();
+                    property.Type.Should().Be("List<string>");
+                }
+                catch (AssertFailedException e)
+                {
+                    return $"[Prop3] {e.Message}";
+                }
+
+                return String.Empty;
+            }
+
+            string? Property4(PropertyClass property)
+            {
+                if (property.Name != "Prop4")
+                {
+                    return null;
+                }
+
+                try
+                {
+                    property.Type.Should().NotBeNullOrEmpty();
+                    property.Type.Should().Be("Skyline.DataMiner.Net.IConnection");
+                }
+                catch (AssertFailedException e)
+                {
+                    return $"[Prop4] {e.Message}";
+                }
+
+                return String.Empty;
+            }
+        }
+
         private class QActionAnalyzer : CSharpAnalyzerBase
         {
             private readonly Action<PropertyClass> check;
